884b1e80f22ab555c020d4eda3dc000428975b59
your name here <nickbielak95@gmail.com>
2018-12-05 14:47:53 -0800

    Wrong file

--- github_grabber.js
+++ github_grabber.js
@@ -1,41 +1,48 @@
-const fs = require('fs')
-const qs = require('querystring')
-const http = require('http')
-const https = require('https')
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const qs = require('querystring');
 
-function buildOptionsObj(username) {
-    return {
-        hostname: `api.github.com`,
-        path: `/users/${username}/starred`,
-        headers: {
-            'User-Agent': 'github-grabber'
-        }
-    }
-}
+const optionObj = username => ({
+  hostname: "https://api.github.com",
+  path: `/users/${username}/starred`,
+  headers: {
+      'User-Agent': 'github-grabber'
+  }
+});
 
-const githubServer = http.createServer((req, res) => {
-    if (req.method === 'POST') {
-        let requestBody = ''
-        req.on('data', chunk => {
-            requestBody += chunk
+const server = http.createServer((req, res) => {
+    if (req.method === "POST") {
+        
+        let body = '';
+        req.on('data', d => {
+            body += d;
         })
+        
         req.on('end', () => {
-            const username = qs.parse(requestBody).username
-            const ws = fs.createWriteStream(`./${username}_starred_repos.txt`)
-            const opts = buildOptionsObj(username)
-            https.get(opts, (dataStream) => {
-                let repoData = ''
-                dataStream.on('data', chunk => { repoData += chunk })
-                dataStream.on('end', () => {
+            const username = qs.parse(body).username;
+            const ws = fs.createWriteStream(`./${username}_starred_repos`);
+            const options = optionObj(username);
+
+            https.get(options, data => {
+                let repoData = '';
+
+                data.on('data', chunk => {
+                    repoData += chunk;
+                })
+
+                data.on('end', () => {
                     const repos = JSON.parse(repoData).map(repo => {
-                        return `Repo: ${repo.name}. Stars: ${repo.stargazers_count}.`
+                        return `Repo: ${repo.name}. Stars: ${repo.stargazer_count}`
                     }).join('\n')
-                    ws.write(repos)
-                    res.end(repos)
+                    ws.write(repos);
+                    res.end(repos);
                 })
             })
         })
+    } else {
+        res.end("DANGER, I am not a POST req");
     }
 })
 
-githubServer.listen(8080, () => console.log('Listening on 8080'))
+server.listen(8080, () => console.log('Listening on Port 8080'));
